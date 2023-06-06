3f840bf3025e8328eee824fcbfa5e2d60d3c5099
Huynh Chi <huynhchi90524@gmail.com>
2023-06-06 11:20:54 +0700

    upload 11h20

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
+const body = $('body')
 
 const popupDownloadElement = $('.popup-download')
 const downloadElement = $('.nav-flex__download')
@@ -8,6 +9,13 @@ const popupNotiElement = $('.popup-noti')
 const notiElement = $('.nav-links__noti')
 const popupLanguageElement = $('.popup-language')
 const languageElement = $('.nav-links__language')
+const searchInputBar = $('.search-input__bar')
+const boxSearchResultBar = $('.box-search-result')
+const searchResultBar = $('.search-result')
+const searchText = $('#search-text')
+const resultSearchText = $('.result-text')
+const popupCardElement = $('.popup-cart')
+const cartElement = $('.wrapper__shopcart i')
 
 
 
@@ -54,10 +62,64 @@ const app = {
         popupLanguageElement.onmouseout = function() {
             popupLanguageElement.classList.remove('popup-active')
         }
+
+        //hover vào giỏ hàng
+        cartElement.onmouseover = function() {
+            popupCardElement.classList.add('popup-active')
+        }
+        cartElement.onmouseout = function() {
+            popupCardElement.classList.remove('popup-active')
+        }
+        popupCardElement.onmouseover = function() {
+            popupCardElement.classList.add('popup-active')
+        }
+        popupCardElement.onmouseout = function() {
+            popupCardElement.classList.remove('popup-active')
+        }
+        
     },
+
+    handleSearchBar() {
+        searchText.onclick = function(e) {
+            searchResultBar.classList.add('search-result-active')
+        }
+        
+        body.onclick = function(e) {
+            searchResultBar.classList.remove('search-result-active')
+        }
+
+        searchInputBar.onclick = function(e) {
+            e.stopPropagation()
+        }
+
+        boxSearchResultBar.onclick = function(e) {
+            e.stopPropagation()
+        }
+
+
+        searchText.placeholder = 'Đăng ký và nhận voucher bạn mới đến 70k!'
+        resultSearchText.innerHTML = searchText.value || searchText.placeholder
+        
+        searchText.onkeyup = function(e) {
+            if (searchText.value != '') {
+                resultSearchText.innerHTML = `
+                    <i class="fas fa-store"></i> Tìm Shop "
+                    ${searchText.value}"
+                `
+                searchResultBar.style.height = 36 + 'px'
+            } else {
+                resultSearchText.innerHTML = searchText.placeholder
+                searchResultBar.style.height = 44 + 'px'
+            }
+        }
+    },
+
+    
 
     start() {
         this.handleHover()
+
+        this.handleSearchBar()
     }
 }
 
